a369c80d49ba160f1df81df1e226abe170c80885
David <David@Davids-MacBook-Pro.local>
2023-02-02 17:16:52 -0600

    week7day3 HW

---
+++ main.js
@@ -0,0 +1,57 @@
+console.log('Pokedex')
+
+
+const getFormData = async (e) => {
+
+
+
+    e.preventDefault();
+    const pokemon = e.target.pokemon.value;
+    console.log(pokemon)
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    // Fetch and Axios are both ways to make HTTP requests in JS
+    // fetch() -- this is built in
+    // axios.get()
+
+    const res = await fetch(url);
+    
+    const data = await res.json();
+    
+
+    const name = data['name'];
+    const id = data.id;
+    const imgUrl = data.sprites.front_default;  
+     
+    const myData =  {
+        name: name,
+        id: id,
+        imgUrl: imgUrl
+    }
+    
+
+    addToPage(myData)
+};
+
+const myForm = document.getElementById('myForm')
+myForm.addEventListener('submit', getFormData)
+
+
+const addToPage = (p) => {
+    console.log(p)
+    const card = document.createElement('div');
+    card.innerHTML = `
+    <div class="card" style="width: 18rem;">
+    <img src="${p.imgUrl}" class="card-img-top" alt="${p.name}">
+    <div class="card-body">
+      <h5 class="card-title">${p.name}</h5>
+    </div>
+  </div>
+    `
+    const container = document.querySelector('.container');
+    if (container.innerHTML !== ''){
+        container.innerHTML = ''
+    }
+    container.append(card)
+};
+
+console.log('hello')
